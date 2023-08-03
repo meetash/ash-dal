4bf17b4a9b43f81b2ee7385bcac51a8d74e11754
Dmitry Marchuk <dmarchuk@remedypointsolutions.com>
2023-08-03 13:22:02 +0200

    Create async paginators

---
+++ ash_dal/utils/paginator/__init__.py
@@ -0,0 +1,9 @@
+from ash_dal.utils.paginator.async_paginator import AsyncDeferredJoinPaginator, AsyncPaginator
+from ash_dal.utils.paginator.sync_paginator import DeferredJoinPaginator, Paginator
+
+__all__ = [
+    "Paginator",
+    "DeferredJoinPaginator",
+    "AsyncPaginator",
+    "AsyncDeferredJoinPaginator",
+]
---
+++ ash_dal/utils/paginator/async_paginator.py
@@ -0,0 +1,58 @@
+import math
+import typing as t
+
+from sqlalchemy import Select, func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.sql.roles import ColumnsClauseRole
+
+T = t.TypeVar("T", bound=DeclarativeBase)
+
+
+class AsyncPaginator(t.Generic[T]):
+    def __init__(self, session: AsyncSession, query: Select[t.Any], page_size: int):
+        self._session = session
+        self._page_size = page_size
+        self._query = query
+
+    async def get_page(self, page_index: int) -> t.Sequence[T]:
+        offset = page_index * self._page_size
+        page_stmt = self._query.offset(offset).limit(self._page_size)
+        result = await self._session.scalars(page_stmt)
+        page: t.Sequence[T] = result.all()
+        return page
+
+    async def paginate(self):
+        current_page = 0
+        while True:
+            page = await self.get_page(page_index=current_page)
+            if not page:
+                break
+            yield page
+            current_page += 1
+
+    @property
+    async def size(self) -> int:
+        """Returns the count of pages the requested resource has"""
+        stmt = select(func.count()).select_from(self._query.subquery())
+        items_count: int = await self._session.scalar(stmt) or 0
+        return math.ceil(items_count / self._page_size)
+
+
+class AsyncDeferredJoinPaginator(AsyncPaginator[T]):
+    def __init__(self, session: AsyncSession, query: Select[t.Any], page_size: int, pk_field: ColumnsClauseRole):
+        super().__init__(session=session, query=query, page_size=page_size)
+        self._pk_field = pk_field
+
+    async def get_page(self, page_index: int) -> t.Sequence[T]:
+        offset = page_index * self._page_size
+        deferred_join_subquery = (
+            self._query.with_only_columns(self._pk_field).offset(offset).limit(self._page_size).subquery()
+        )
+        stmt = self._query.join(
+            target=deferred_join_subquery,
+            onclause=self._pk_field == deferred_join_subquery.c[0],  # pyright: ignore [reportGeneralTypeIssues]
+        )
+        result = await self._session.scalars(stmt)
+        page: t.Sequence[T] = result.all()
+        return page
