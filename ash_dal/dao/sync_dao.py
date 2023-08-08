ab2637e2dd4df3273c63cd8ee5c52d4b9d96a381
Dmitry Marchuk <dmarchuk@remedypointsolutions.com>
2023-08-08 11:43:12 +0200

    Add docstrings for DAOs

--- ash_dal/dao/sync_dao.py
+++ ash_dal/dao/sync_dao.py
@@ -20,10 +20,19 @@ class BaseDAO(BaseDAOMixin[Entity]):
 
     @property
     def db(self) -> Database:
+        """
+        Property returns an instance of :class:`Database` class
+        :return: DB instance
+        """
         assert hasattr(self, "_db")
         return self._db
 
     def get_by_pk(self, pk: t.Any) -> Entity | None:
+        """
+        Using this method you can fetch an entity by its primary key
+        :param pk: the record's primary key value
+        :return: Entity instance or None if the record is not found
+        """
         with self.db.session as session:
             db_item = session.get(self.__model__, pk)
             if not db_item:
@@ -31,6 +40,10 @@ class BaseDAO(BaseDAOMixin[Entity]):
             return self._convert_db_item_in_entity(db_item=db_item)
 
     def all(self) -> tuple[Entity, ...]:
+        """
+        Using this method you can fetch all entities from the database
+        :return: a tuple with entities
+        """
         with self.db.session as session:
             db_items = session.scalars(select(self.__model__))
             return self._get_entities_from_db_items(db_items=db_items)
@@ -40,6 +53,12 @@ class BaseDAO(BaseDAOMixin[Entity]):
         page_index: int = 0,
         page_size: int | None = None,
     ) -> PaginatorPage[Entity]:
+        """
+        Fetch a page with entities by page index. If the index is out of range, an empty page will be returned.
+        :param page_index: Numeric value. Index starts from 0
+        :param page_size: Numeric value. Defines size of the page that will be returned
+        :return: An instance of :class:`PaginatorPage` that includes entities.
+        """
         with self.db.session as session:
             paginator = self._config.paginator_class(
                 session=session, query=select(self.__model__), page_size=page_size or self._config.default_page_size
@@ -53,6 +72,12 @@ class BaseDAO(BaseDAOMixin[Entity]):
         specification: dict[str, t.Any] | None = None,
         page_size: int | None = None,
     ) -> t.Iterator[PaginatorPage[Entity]]:
+        """
+        An iterator that returns pages with entities.
+        :param specification: Can be used to filter the entities you want to receive.
+        :param page_size: Numeric value. Defines size of pages that will be returned
+        :return: :class:`t.Iterator` that returns :class:`PaginatorPage` with entities
+        """
         with self.db.session as session:
             query = select(self.__model__)
             if specification:
@@ -67,11 +92,21 @@ class BaseDAO(BaseDAOMixin[Entity]):
                 yield PaginatorPage(index=page_index, items=entities)
 
     def filter(self, specification: dict[str, t.Any]) -> tuple[Entity, ...]:
+        """
+        Fetches entities from database by specification.
+        :param specification: a dict that will be used for filtering
+        :return: a tuple with entities
+        """
         with self.db.session as session:
             db_items = session.scalars(select(self.__model__).filter_by(**specification))
             return self._get_entities_from_db_items(db_items=db_items)
 
     def create(self, data: dict[str, t.Any]) -> Entity:
+        """
+        Creates an entity in database
+        :param data: a dict that represents entity to be created.
+        :return: a created entity instance
+        """
         with self.db.session as session:
             result = session.execute(insert(self.__model__).values(**data))
             session.commit()
@@ -80,11 +115,22 @@ class BaseDAO(BaseDAOMixin[Entity]):
             return self._dict_to_entity(dict_=response_data)
 
     def bulk_create(self, data: t.Sequence[dict[str, t.Any]]):
+        """
+        Create multiple records by one request
+        :param data: a sequence with dicts that represent entities to be created
+        """
         with self.db.session as session:
             session.execute(insert(self.__model__), data)
             session.commit()
 
     def update(self, specification: dict[str, t.Any], update_data: dict[str, t.Any]) -> bool:
+        """
+        Patches record(s)
+        :param specification: record(s) for updating are chosen based on this specification. If an empy dict is passed,
+        a :class:`ValueError` exception will be raised for safety reasons.
+        :param update_data: a dict with new values to be written for the chosen record(s)
+        :return: a :class:`bool` value that shows either the entity(ies) are updated or not.
+        """
         if not specification:
             raise ValueError("Specification should be passed")
         with self.db.session as session:
@@ -93,6 +139,12 @@ class BaseDAO(BaseDAOMixin[Entity]):
             return bool(result.rowcount)  # pyright: ignore
 
     def delete(self, specification: dict[str, t.Any]) -> bool:
+        """
+        Removes record(s).
+        :param specification: record(s) for removing are chosen based on this specification. If an empy dict is passed,
+        a :class:`ValueError` exception will be raised for safety reasons.
+        :return: a :class:`bool` value that shows either the entity(ies) are removed or not.
+        """
         if not specification:
             raise ValueError("Specification should be passed")
         with self.db.session as session:
