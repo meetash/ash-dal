69735f5032c348391687526517bba91ef730ba9d
Dmitry Marchuk <78796238+dmitry-engineer@users.noreply.github.com>
2023-09-05 13:10:09 +0200

    Update __init__.py

--- ash_dal/__init__.py
+++ ash_dal/__init__.py
@@ -4,7 +4,7 @@ from ash_dal.dao import AsyncBaseDAO, BaseDAO
 from ash_dal.database import AsyncDatabase, Database
 from ash_dal.utils import AsyncDeferredJoinPaginator, AsyncPaginator, DeferredJoinPaginator, Paginator, PaginatorPage
 
-__VERSION__ = "0.1.0"
+__VERSION__ = "0.1.1"
 
 
 __all__ = [
